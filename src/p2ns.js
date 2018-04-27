a25cdd01157fac06ca2e07006d6c8330409ccca9
Fan Jiang <fanjiang@thoughtworks.com>
2018-04-28 00:38:26 +0800

    dirty dependency

---
+++ src/p2ns.js
@@ -0,0 +1,50 @@
+import contract from "truffle-contract";
+import P2NSJSON from "./contracts/P2NS.json";
+
+class P2NS {
+  constructor(web3) {
+    this.web3 = web3
+    this.contract = contract(P2NSJSON);
+    this.contract.setProvider(web3.currentProvider);
+  }
+
+  async myAddress() {
+    return (await this.web3.eth.getAccounts())[0]
+  }
+
+  async p2nsContract() {
+    let currentNetwork = this.web3.currentProvider.publicConfigStore._state.networkVersion
+    let exist
+    if(currentNetwork === "1") { // mainnet
+      exist = await this.contract.at('0x0')
+    } else if (currentNetwork === "3") {  // ropsten
+      exist = await this.contract.at('0x0')
+    } else if (currentNetwork === "4") { // rinkeby
+      exist = await this.contract.at('0xe9880eedd4d44ec0b25f3a362af5b8dfd2754c62')
+    } else {
+      exist = await this.contract.deployed()
+    }
+    return exist
+  }
+
+  putName = async (value) => {
+    let exist = await this.p2nsContract()
+    let owner = await this.myAddress()
+
+    return await exist.PutName(value, {from: owner})
+  }
+
+  addressOf = async (value) => {
+    let exist = await this.p2nsContract()
+    let owner = await this.myAddress()
+    return await exist.AddressOf(value, {from: owner})
+  }
+
+  nameOf = async (value) => {
+    let exist = await this.p2nsContract()
+    let owner = await this.myAddress()
+    return await exist.NameOf(value, {from: owner})
+  }
+}
+
+export default P2NS
