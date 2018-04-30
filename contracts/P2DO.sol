6c94812c7a5478ff5f9f7228eb8806c1812854f8
Fan Jiang <fanjiang@thoughtworks.com>
2018-04-30 16:50:58 +0800

    Revert "add test for new a post"
    This reverts commit c036406d1725d95fedcb7fe2ee0ca7d72efa3687.

--- contracts/P2DO.sol
+++ contracts/P2DO.sol
@@ -14,8 +14,6 @@ contract P2DO {
         address author;
         string content;
     }
-
-    event NewPost(address indexed _author, string _content, uint256 _postNum);
     /*
     NOTE:
     The following variables are OPTIONAL vanities. One does not have to include them.
@@ -23,22 +21,23 @@ contract P2DO {
     Some wallets/interfaces might not even bother to look at this information.
     */
 
-    string public version = "H0.1";       //human 0.1 standard. Just an arbitrary versioning scheme.
+    string public version = 'H0.1';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
-    function newPost(string _content) public {
+    function NewPost(string _content) public returns (uint256 _postNum){
         posts[postNum] = Post(msg.sender, _content);
-        emit NewPost(msg.sender, _content, postNum++);
+        postNum++;
+        return _postNum;
     }
 
-    function getPostAuthor(uint256 _postIndex) public view returns (address _postAuthor){
+    function GetPostAuthor(uint256 _postIndex) public view returns (address _postAuthor){
         return posts[_postIndex].author;
     }
 
-    function getPostContent(uint256 _postIndex) public view returns (string _postContent){
+    function GetPostContent(uint256 _postIndex) public view returns (string _postContent){
         return posts[_postIndex].content;
     }
 
-    function getPostNum() public view returns (uint256 _postNum){
+    function GetPostNum() public view returns (uint256 _postNum){
         return postNum;
     }
 }
